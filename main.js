5c13451136ada11265b3fccaf93b77dfe14ee40b
hevelin32 <133216434+hevelin32@users.noreply.github.com>
2024-04-23 08:16:29 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 var jogador = "x";
 
+var jg1 = []
+var jg2 = []
+
+const listaDeVitorias = [
+
+]
+
 function jogar(celula){
     if(celula.innerHTML == ""){
         celula.innerHTML = jogador;
-
         if(jogador == "x"){
+            jg1.push(celula);
+            console.log(jg1)
             jogador = "o";
         } else{
             jogador = "x"
+            jg2.push(celula);
+            console.log(jg2)
         }
     }
 
